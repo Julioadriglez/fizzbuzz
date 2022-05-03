2819b23f9154ba093e3894eca5b68b9b65c7096e
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-02 20:08:01 -0500

    Se agrgo folder lib y dentro folder servicescon con ExplorerService.js y folder utils con Reader.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 // Part 1 Read json file ===========================
 const rawdata = fs.readFileSync("explorers.json");
 const explorers = JSON.parse(rawdata);
-
+console.log(explorers)
 // Part 2: Get the quantity of explorers names in node
 const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
 //console.log(explorersInNode.length)
