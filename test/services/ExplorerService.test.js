8723b23b46967a1fcbba03202e7444af9c43ef80
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-03 19:15:10 -0500

    Se comento todo index

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -3,7 +3,7 @@ const ExplorerServices = require('./../../lib/services/ExplorerServices')
 describe("Pruebasa en ExplorerServices", () => {
     test("1. calcular todos los explorers en una misión", () => {
         const explorers = [{mission: "node"}, {mission: "node"}];
-        const explorersInNode = ExplorerServices.filterByMission(explorers, "node");
-        expect(explorersInNode.length).toBe(2);
+        const explorersInNode = ExplorerServices.filterByMission(explorers, "node")
+        expect(explorersInNode.length).toBe(2)
     })
 })
