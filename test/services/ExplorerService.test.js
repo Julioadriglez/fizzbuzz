91003eaa79fafb2d1e403583e6c3030145f596b3
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-04 18:29:16 -0500

    Se agrgo tercera pruena para Reader

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -3,16 +3,21 @@ const Reader= require('./../../lib/utils/Reader')
 const ExplorerServices = require('./../../lib/services/ExplorerServices')
 
 describe("Pruebasa en ExplorerServices", () => {
-    test("1. calcular todos los explorers en una misión", () => {
+    test("1. prueba de reader", () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+        expect(explorers).not.toBeUndefined();
+    })
+    test("2. calcular todos los explorers en una misión", () => {
         const explorers = [{"name": "explorer1","githubUsername": "ajolonauta1","score": 1,"mission": "node"},{"name": "explorer2","githubUsername": "ajolonauta2","score": 1,"mission": "java"}];
         const explorersInNode = ExplorerServices.getAmountOfExplorersByMission(explorers,'node')
         expect(explorersInNode).toBe("Numero de exploradores en mision node: 1")
     });
-    test("2. Comparar si regresa lo sexplorers son de node",() => {
+    test("3. Comparar si regresa lo sexplorers son de node",() => {
         const explorers = Reader.readJsonFile("explorers.json")
         const usernamesInNode = ExplorerServices.filterByMission(explorers, "node")
         expect(usernamesInNode[0].mission).toBe('node')
         expect(usernamesInNode[4].mission).toBe('node')
         expect(usernamesInNode[9].mission).toBe('node')
-    })
+    });
+
 })
