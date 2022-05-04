1212a20a179051131c649590bfc46a4a434ba776
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-03 19:11:22 -0500

    se comento todo el app

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const ExplorersServices = require('./lib/services/ExplorerServices')
 const explorers = Reader.readJsonFile("explorers.json")
 
 
-console.log(ExplorersServices.filterByMission(explorers, 'node'));
+console.log(ExplorersServices.filterByMission(explorers, 'node'))
 console.log(ExplorersServices.getAmountOfExplorersByMission(explorers,'node'))
