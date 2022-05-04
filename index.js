69eb100771be6e766b94982f06e5129639ce58c7
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-03 19:29:40 -0500

    se reinstalo jest

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// const Reader= require('./lib/utils/Reader')
-// const ExplorersServices = require('./lib/services/ExplorerServices')
-// const explorers = Reader.readJsonFile("explorers.json")
+const Reader= require('./lib/utils/Reader')
+const ExplorersServices = require('./lib/services/ExplorerServices')
+const explorers = Reader.readJsonFile("explorers.json")
 
 
-// console.log(ExplorersServices.filterByMission(explorers, 'node'))
-// console.log(ExplorersServices.getAmountOfExplorersByMission(explorers,'node'))
+console.log(ExplorersServices.filterByMission(explorers, 'node'))
+console.log(ExplorersServices.getAmountOfExplorersByMission(explorers,'node'))
