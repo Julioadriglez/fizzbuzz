e04d71e0321db20637f260040d9890358b4be8a0
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-05 21:58:09 -0500

    Se aplicaron las correciones de eslin

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,6 +1,6 @@
 
-const Reader= require('./../../lib/utils/Reader')
-const ExplorerServices = require('./../../lib/services/ExplorerServices')
+const Reader= require("./../../lib/utils/Reader");
+const ExplorerServices = require("./../../lib/services/ExplorerServices");
 
 describe("Pruebasa en ExplorerServices y Reader", () => {
     test("1. prueba de reader", () => {
@@ -9,23 +9,23 @@ describe("Pruebasa en ExplorerServices y Reader", () => {
     });
     test("2. calcular todos los explorers en una misión", () => {
         const explorers = [{"name": "explorer1","githubUsername": "ajolonauta1","score": 1,"mission": "node"},{"name": "explorer2","githubUsername": "ajolonauta2","score": 1,"mission": "java"}];
-        const explorersInNode = ExplorerServices.getAmountOfExplorersByMission(explorers,'node')
-        expect(explorersInNode).toBe("Numero de exploradores en mision node: 1")
+        const explorersInNode = ExplorerServices.getAmountOfExplorersByMission(explorers,"node");
+        expect(explorersInNode).toBe("Numero de exploradores en mision node: 1");
     });
     test("3. Comparar si los explorers son de node",() => {
-        const explorers = Reader.readJsonFile("explorers.json")
-        const usernamesInNode = ExplorerServices.filterByMission(explorers, "node")
-        expect(usernamesInNode[0].mission).toBe('node')
-        expect(usernamesInNode[4].mission).toBe('node')
-        expect(usernamesInNode[9].mission).toBe('node')
+        const explorers = Reader.readJsonFile("explorers.json");
+        const usernamesInNode = ExplorerServices.filterByMission(explorers, "node");
+        expect(usernamesInNode[0].mission).toBe("node");
+        expect(usernamesInNode[4].mission).toBe("node");
+        expect(usernamesInNode[9].mission).toBe("node");
     });
     test("4. retorno de usernames correctos", () => {
         const explorers = [{"name": "explorer1", "githubUsername":"ajolonauta1", "score":1,"mission": "node"},{"name": "explorer2", "githubUsername":"ajolonauta2", "score":5, "mission": "java"},{"name": "explorer3", "githubUsername":"ajolonauta3", "score":12,"mission": "node"}];
-        const explorersInNode = ExplorerServices.getExplorersUsernamesByMission(explorers, "node")
-        console.log(explorersInNode)
-        expect(explorersInNode).toContain("ajolonauta1")
-        expect(explorersInNode).toContain("ajolonauta3")
+        const explorersInNode = ExplorerServices.getExplorersUsernamesByMission(explorers, "node");
+        console.log(explorersInNode);
+        expect(explorersInNode).toContain("ajolonauta1");
+        expect(explorersInNode).toContain("ajolonauta3");
 
-    })
+    });
 
-})
+});
--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,26 +1,26 @@
-const FizzbuzzService= require('./../../lib/services/FizzbuzzService')
+const FizzbuzzService= require("./../../lib/services/FizzbuzzService");
 const fs = require("fs");
 
 describe("Puevas de unidad para FizzbuzzServices", () => {
     test("1. Prueba no divisible entre 3 y 5", () => {
-        const explorer1 = {"name": "Explorer1", "score": 1}
-        const explorerscore = FizzbuzzService.applyValidationInExplorer(explorer1)
-        expect(explorerscore.trick).toBe(1)
+        const explorer1 = {"name": "Explorer1", "score": 1};
+        const explorerscore = FizzbuzzService.applyValidationInExplorer(explorer1);
+        expect(explorerscore.trick).toBe(1);
     });
     test("2. Prueba divisible entre 3 Fizz", () =>{
-        const explorer3 = {name: "Explorer3", score: 3}
-        const explorerscore = FizzbuzzService.applyValidationInExplorer(explorer3)
-        expect(explorerscore.trick).toBe("FIZZ")
+        const explorer3 = {name: "Explorer3", score: 3};
+        const explorerscore = FizzbuzzService.applyValidationInExplorer(explorer3);
+        expect(explorerscore.trick).toBe("FIZZ");
     });
     test("3. Prueba divisible entre 5 BUZZ", () => {
-        const explorer5 = {name: "Explorer5", score: 5}
-        const explorerscore = FizzbuzzService.applyValidationInExplorer(explorer5)
-        expect(explorerscore.trick).toBe("BUZZ")
+        const explorer5 = {name: "Explorer5", score: 5};
+        const explorerscore = FizzbuzzService.applyValidationInExplorer(explorer5);
+        expect(explorerscore.trick).toBe("BUZZ");
     });
     test("4. Prueba divisible entre 5 y 3 FIZZBUZZ", () => {
-        const explorer15 = {name: "Explorer15", score: 15}
-        const explorerscore = FizzbuzzService.applyValidationInExplorer(explorer15)
-        expect(explorerscore.trick).toBe("FIZZBUZZ")
-    })
+        const explorer15 = {name: "Explorer15", score: 15};
+        const explorerscore = FizzbuzzService.applyValidationInExplorer(explorer15);
+        expect(explorerscore.trick).toBe("FIZZBUZZ");
+    });
 
-})
+});
