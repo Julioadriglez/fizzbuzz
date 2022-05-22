9350640092ec0ec4cda1b37d22f412628b8e175f
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-22 17:09:53 -0500

    se creo archivo bot.js y se realizaron correcciones en FizzbuzzService.test.js

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,4 +1,4 @@
-const FizzbuzzService= require("./../../lib/services/FizzbuzzService");
+const FizzbuzzService= require("./../../lib/services/FizzbuzzService")
 
 describe("Puevas de unidad para FizzbuzzServices", () => {
     test("1. Prueba no divisible entre 3 y 5", () => {
